53322d495925cf73519b0f225d0061d9d9c532c0
Yash_dev_01 <yashagarwal0445@gmail.com>
2025-07-30 13:39:10 +0530

    fixd stars issuee

--- server.js
+++ server.js
@@ -20,15 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-//app.options('/api/*', cors()); // optional, for preflight
-
+app.use(cors());
 app.use(json());
 
 // DB Connection
